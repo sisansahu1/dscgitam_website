36d8ee330fce58f1e6879699150f5e076226ef22
Sisan Sahu <sisansahu1@gmail.com>
2020-01-01 18:11:31 +0530

    v1.0
    v1.0  (Beta)
    
    Co-Authored-By: Ashish Raju <asishraju@users.noreply.github.com>

---
+++ animate.js
@@ -0,0 +1,41 @@
+// Some random colors
+const colors = ["#DB4437", "#4285F4", "#F4B400", "#0F9D58"];
+
+const numBalls = 40;
+const balls = [];
+
+for (let i = 0; i < numBalls; i++) {
+  let ball = document.createElement("div");
+  ball.classList.add("ball");
+  ball.style.background = colors[Math.floor(Math.random() * colors.length)];
+  ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
+  ball.style.top = `${Math.floor(Math.random() * 100)}vh`;
+  ball.style.transform = `scale(${Math.random()})`;
+  ball.style.width = `${Math.random()}em`;
+  ball.style.height = ball.style.width;
+  
+  balls.push(ball);
+  document.body.append(ball);
+}
+
+// Keyframes
+balls.forEach((el, i, ra) => {
+  let to = {
+    x: Math.random() * (i % 2 === 0 ? -11 : 11),
+    y: Math.random() * 12
+  };
+
+  let anim = el.animate(
+    [
+      { transform: "translate(0, 0)" },
+      { transform: `translate(${to.x}rem, ${to.y}rem)` }
+    ],
+    {
+      duration: (Math.random() + 1) * 2000, // random duration
+      direction: "alternate",
+      fill: "both",
+      iterations: Infinity,
+      easing: "ease-in-out"
+    }
+  );
+});
---
+++ events_page/README.md
@@ -0,0 +1,17 @@
+# Responsive Vertical Timeline
+
+An easy to customize, responsive timeline.
+
+[Article on CodyHouse](https://codyhouse.co/gem/vertical-timeline)
+
+[Demo](https://codyhouse.co/demo/vertical-timeline)
+ 
+[License](https://codyhouse.co/license)
+
+## Dependencies
+
+This experiment is built upon the [CodyHouse Framework](https://github.com/CodyHouse/codyhouse-framework).
+
+## Credits
+
+Icons: [Nucleo Library](https://nucleoapp.com/)
